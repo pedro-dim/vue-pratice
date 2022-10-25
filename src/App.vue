c5dd86dbd5e9e8f592224810d457a593642f6e47
pedro-dim <pcontato19@gmail.com>
2022-10-25 15:42:11 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Test from "./components/Test.vue";
+
+</script>
+
+<template>
+
+  <Test message="Vai tomar no cu" outro="Poha nenhuma"/>
+
+</template>
+
+<style scoped>
+
+</style>
